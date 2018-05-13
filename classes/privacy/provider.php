134806727220d41836b0eee6c17fc3bc6d21ae3b
Charles Fulton <fultonc@lafayette.edu>
2018-05-13 10:01:50 -0400

    Fix whitespace

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -26,10 +26,10 @@ namespace local_metasync\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
-class provider implements 
+class provider implements
     // This plugin does not store any personal user data.
     \core_privacy\local\metadata\null_provider {
- 
+
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
